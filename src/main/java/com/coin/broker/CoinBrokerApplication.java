2c2cd5324d77bd6e0e7024f3bb66d39508fc1739
parkJs <plannist19@gmail.com>
2024-01-09 17:47:48 +0900

    first commit

---
+++ src/main/java/com/coin/broker/CoinBrokerApplication.java
@@ -0,0 +1,13 @@
+package com.coin.broker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinBrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinBrokerApplication.class, args);
+	}
+
+}
